45aea1facbc20d4a86e0452e32e74c8518ba002a
dewey <d.thorne@gmail.com>
2014-04-07 11:56:40 -0600

    added configurations

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -1,11 +1,12 @@
 var db = require('../providers/mongo-db-provider');
+var _ = require('underscore');
 
 var keys = ['name', 'content'];
 
 module.exports = function(app){
   
   //get all reports
-  app.get('/reports', function(request, response){
+  app.get(app.get('endpoint') + '/reports', function(request, response){
     db.getAllReports(function(reports, error) {
         if(error) {
             console.log(error);
@@ -17,25 +18,28 @@ module.exports = function(app){
   });
 
 //get all reports
-  app.post('/reports', function(request, response) {
+  app.post(app.get('endpoint') + '/reports', function(request, response) {
     var report = request.body;
-    verfiyReport(report);
-    report = _.pick(report, keys);
-    db.insertReport(report, function(report, error) {
-        if(error) {
-            console.log('error', error);
-            response.send(404, error);
-        } else {
-            response.send(report);
-        }
-    });    
 
+    if(verifyReport(report)) {
+        //report = _.pick(report, keys);
+        db.insertReport(report, function(report, error) {
+            if(error) {
+                console.log('error', error);
+                response.send(404, error);
+            } else {
+                response.send(report);
+            }
+        });    
+    } else {
+        response.send(400, "Response was not properly validated");
+    }
   });
 
     function verifyReport(report) {
         var valid = true;
         _.forEach(keys, function(key) {
-            valid = report.has(key);
+            valid = _(report).has(key);
         });
         return valid;
     }
