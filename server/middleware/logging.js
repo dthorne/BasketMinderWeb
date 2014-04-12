211db45c9378766cdf4cef611387132baf3b12eb
dewey <d.thorne@gmail.com>
2014-04-11 22:49:38 -0600

    removed logging.js middleware

--- server/middleware/logging.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-  return function(request, response, next){
-    if(request.headers.password && request.headers.password == 'tankjig') {
-        next();
-    } else {
-        response.send(403, "Access Denied");    
-    }
-  };
-};
