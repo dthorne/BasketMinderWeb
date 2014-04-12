f2ee55a492650c526336a1377aaa9704e2c8d48f
Todd <todd@todd-VirtualBox.(none)>
2014-04-12 11:43:50 -0600

    adding the app.js changes

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,6 @@ angular.module('app', ['ngResource']).
     //setup URL routes
     $routeProvider.when('/home', {templateUrl: 'templates/home.html'});
     $routeProvider.when('/about', {templateUrl: 'templates/about.html'});
-    $routeProvider.when('/view2', {templateUrl: 'templates/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/feedback', {templateUrl: 'templates/feedback.html'});
+    $routeProvider.otherwise({redirectTo: '/home'});
   }]);
