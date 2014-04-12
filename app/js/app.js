41e27e3267d208157291db9dec0a1daec3d9399c
dewey <d.thorne@gmail.com>
2014-04-12 10:25:57 -0600

    added logo to the home screen, added the basic funcitioning tabs. Added text to home and about screens.

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,8 @@
 angular.module('app', ['ngResource']).
   config(['$routeProvider', function($routeProvider) {
     //setup URL routes
-    $routeProvider.when('/view1', {templateUrl: 'templates/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/home', {templateUrl: 'templates/home.html'});
+    $routeProvider.when('/about', {templateUrl: 'templates/about.html'});
     $routeProvider.when('/view2', {templateUrl: 'templates/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
