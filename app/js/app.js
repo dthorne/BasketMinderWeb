38eb763742afd801af4626c7ae28c76ee3d60bb4
dewey <d.thorne@gmail.com>
2014-04-16 16:36:53 -0600

    changes to about.html

--- app/js/app.js
+++ app/js/app.js
@@ -5,5 +5,5 @@ angular.module('app', ['ngResource']).
     $routeProvider.when('/home', {templateUrl: 'templates/home.html'});
     $routeProvider.when('/about', {templateUrl: 'templates/about.html'});
     $routeProvider.when('/view2', {templateUrl: 'templates/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/home'});
   }]);
